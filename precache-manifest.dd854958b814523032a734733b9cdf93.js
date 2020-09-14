5fc7f28e67264f54d0bef8d7a5517b3a4016d4e9
M <matinrosyid@live.com>
2020-09-14 14:54:10 +0100

    Updates

---
+++ precache-manifest.dd854958b814523032a734733b9cdf93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c1c851f2731779631c2b568868cf39f",
+    "url": "/PortfolioSite/index.html"
+  },
+  {
+    "revision": "556f222f021f528ae1e1",
+    "url": "/PortfolioSite/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a35aab8cf65e99001189",
+    "url": "/PortfolioSite/static/css/main.150c4107.chunk.css"
+  },
+  {
+    "revision": "556f222f021f528ae1e1",
+    "url": "/PortfolioSite/static/js/2.e7a9034b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PortfolioSite/static/js/2.e7a9034b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35aab8cf65e99001189",
+    "url": "/PortfolioSite/static/js/main.207de366.chunk.js"
+  },
+  {
+    "revision": "aa0fb92b507b1de6669c",
+    "url": "/PortfolioSite/static/js/runtime-main.4eb251b0.js"
+  },
+  {
+    "revision": "6693a378f931465027f6232312039a85",
+    "url": "/PortfolioSite/static/media/colormemorygame.6693a378.jpg"
+  },
+  {
+    "revision": "5b1e6588fdb00ae40336c0c109de9119",
+    "url": "/PortfolioSite/static/media/mountainswebsite.5b1e6588.jpg"
+  },
+  {
+    "revision": "dec27843eead11fbc153af409447194b",
+    "url": "/PortfolioSite/static/media/patatap.dec27843.jpg"
+  }
+]);
